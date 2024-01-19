b58e8321695f32384137c20eaa4c045f920274d2
Daniel McDonald <daniel@mcd.onald.net>
2024-01-19 10:14:10 -0600

    reduce scope of the call, just connect to number provided OOB

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bytes"
 	"fmt"
-	"net/http"
 	"os"
 	"strings"
 
@@ -19,78 +18,48 @@ func formatPhone(number string) string {
 }
 
 var funcs = map[string]any{"formatPhone": formatPhone}
-var forwardTemplate = template.Must(template.New("forward").Funcs(funcs).Parse(`<?xml version="1.0" encoding="UTF-8"?>
+var createCallTemplate = template.Must(template.New("createCall").Funcs(funcs).Parse(`<?xml version="1.0" encoding="UTF-8"?>
 <Response>
-	<Say>Connecting you to number {{formatPhone .Callee}}. Please wait.</Say>
-	<Dial callerId="{{.Caller}}">{{.Callee}}</Dial>
+	<Say>Welcome to the reverse calling system. You will be connected to the number {{formatPhone .ConnectTo}}.</Say>
+	<Dial callerId="{{.DialFirst}}">{{.ConnectTo}}</Dial>
 </Response>`))
 
-type forwardParams struct {
-	Caller string
-	Callee string
+type createCallParams struct {
+	DialFirst string
+	ConnectTo string
 }
 
-func handleForward(resp http.ResponseWriter, req *http.Request) {
-	err := req.ParseForm()
+func setupCall(client *twilio.RestClient, dialFirst string, connectTo string) (*twilioApi.ApiV2010Call, error) {
+	var twiml bytes.Buffer
+	err := createCallTemplate.ExecuteTemplate(&twiml, "createCall", createCallParams{DialFirst: dialFirst, ConnectTo: connectTo})
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
-	fmt.Printf("forward: %v\n", req.Form)
-	params := forwardParams{Caller: req.FormValue("To"), Callee: req.FormValue("Digits")}
-	var buf bytes.Buffer
-	err = forwardTemplate.ExecuteTemplate(&buf, "forward", params)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(buf.String())
-	resp.Write(buf.Bytes())
-}
-
-func handleStatusCallback(resp http.ResponseWriter, req *http.Request) {
-	err := req.ParseForm()
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("status callback: %v\n", req.Form)
-}
-
-func httpServer(stop chan struct{}) {
-	http.HandleFunc("/forward", handleForward)
-	http.HandleFunc("/statusCallback", handleStatusCallback)
-	fmt.Println("Starting http listener")
-	if err := http.ListenAndServe(":8888", nil); err != nil {
-		panic(err)
-	}
-	stop <- struct{}{}
-}
-
-func main() {
-	serverStop := make(chan struct{})
-	go httpServer(serverStop)
-	client := twilio.NewRestClient()
 	params := &twilioApi.CreateCallParams{}
-	if len(os.Args) != 2 {
-		fmt.Println("Usage: reversecalling <number>")
-		os.Exit(1)
-	}
-	callee := os.Args[1]
-	params.SetTo(callee)
+	params.SetTo(dialFirst)
 	params.SetFrom("+14803264574")
-	params.SetTwiml(
-		`<?xml version="1.0" encoding="UTF-8"?>
-<Response>
-	<Say>Welcome to the reverse calling system. You will be connected to the number that you enter. Please enter the number to dial now.</Say>
-	<Gather timeout="15" input="dtmf" action="http://agency.onald.net:8888/forward"/>
-</Response>`)
-	params.SetStatusCallback("http://agency.onald.net:8888/statusCallback")
+	params.SetTwiml(twiml.String())
+	params.SetMachineDetection("Enable")
 
 	resp, err := client.Api.CreateCall(params)
 	if err != nil {
-		fmt.Println(err.Error())
-	} else {
-		fmt.Println("Call Status: " + *resp.Status)
-		fmt.Println("Call Sid: " + *resp.Sid)
-		fmt.Println("Call Direction: " + *resp.Direction)
+		return nil, err
 	}
-	<-serverStop
+	return resp, nil
+}
+
+func main() {
+	client := twilio.NewRestClient()
+	if len(os.Args) != 3 {
+		fmt.Println("Usage: reversecalling <dial-first-number> <connect-to-number>")
+		os.Exit(1)
+	}
+	dialFirst := os.Args[1]
+	connectTo := os.Args[2]
+	call, err := setupCall(client, dialFirst, connectTo)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Println("Call SID: ", *call.Sid)
 }
